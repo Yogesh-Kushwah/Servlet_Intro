029006523b7e4818e0e382ddfc88c742fbd1f768
Yogesh Kushwah <YogeshKushwah775@gmail.com>
2025-02-21 11:11:46 +0530

    [Yogesh Kushwah] Add. First Servlet,Hello World Using Tomcat

---
+++ Servlet_Intro/UC1/firstservlet/api/src/test/java/myfirst/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package myfirst.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
